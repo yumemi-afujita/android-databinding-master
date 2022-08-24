f275d04aa2ce5e01763b0f106e24cf03b0bbc170
Akiko Fujita <a_fujita@yumemi.co.jp>
2022-08-24 10:45:23 +0900

    DataBinding有効化、BindingAdapterの使用

--- app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
@@ -27,10 +27,12 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.databinding.basicsample.R
 import com.example.android.databinding.basicsample.data.Popularity
 import com.example.android.databinding.basicsample.data.SimpleViewModel
+import com.example.android.databinding.basicsample.databinding.PlainActivityBinding
 
 /**
  * Plain old activity with lots of problems to fix.
@@ -43,70 +45,12 @@ class PlainOldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.plain_activity)
+        // <data>ブロック変数で定義されたレイアウト変数を設定
+        val binding: PlainActivityBinding =
+            DataBindingUtil.setContentView(this, R.layout.plain_activity)
 
-        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
-        updateName()
-        updateLikes()
-    }
-
-    /**
-     * This method is triggered by the `android:onclick` attribute in the layout. It puts business
-     * logic in the activity, which is not ideal. We should do something about that.
-     */
-    fun onLike(view: View) {
-        viewModel.onLike()
-        updateLikes()
-    }
-
-    /**
-     * So much findViewById! We'll fix that with Data Binding.
-     */
-    private fun updateName() {
-        findViewById<TextView>(R.id.plain_name).text = viewModel.name
-        findViewById<TextView>(R.id.plain_lastname).text = viewModel.lastName
-    }
-
-    /**
-     * This method has many problems:
-     * - It's calling findViewById multiple times
-     * - It has untestable logic
-     * - It's updating two views when called even if they're not changing
-     */
-    private fun updateLikes() {
-        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
-        findViewById<ProgressBar>(R.id.progressBar).progress =
-            (viewModel.likes * 100 / 5).coerceAtMost(100)
-        val image = findViewById<ImageView>(R.id.imageView)
-
-        val color = getAssociatedColor(viewModel.popularity, this)
-
-        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
-
-        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
-    }
-
-    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
-        return when (popularity) {
-            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
-                intArrayOf(android.R.attr.colorForeground)
-            ).getColor(0, 0x000000)
-            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
-            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
-        }
-    }
-
-    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
-        return when (popularity) {
-            Popularity.NORMAL -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
-            }
-            Popularity.POPULAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-            Popularity.STAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-        }
+        // どのライフサイクルのオーナーを使うかを指定
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
     }
 }
